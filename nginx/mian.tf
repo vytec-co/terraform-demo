1d33417fd0685ff756c8e1097f9e4cb0ddfe9fe9
vsm <devops.july2.2017@gmail.com>
2022-07-27 08:26:46 +0530

    Create mian.tf

---
+++ nginx/mian.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "app_server" {
+  ami           = "ami-068257025f72f470d"
+  instance_type = "t2.micro"
+  key_name = "mumbai"
+  user_data = data.template_file.init.rendered
+  tags = {
+    Name = "nginx"
+  }
+}
+
+data "template_file" "init" {
+  template = "${file("script.sh")}"
+
+  vars = {
+    device_name = "test"
+  }
+}
