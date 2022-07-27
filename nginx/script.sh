b741d6a8fbd2a9f7d98721b50c45185e8f3a59b1
vsm <devops.july2.2017@gmail.com>
2022-07-27 08:27:13 +0530

    Create script.sh

---
+++ nginx/script.sh
@@ -0,0 +1,40 @@
+sudo apt update
+sudo apt install nginx
+
+cd /var/www
+sudo mkdir tutorial
+cd tutorial
+cat <<EOF >index.html
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Hello, Nginx!</title>
+</head>
+<body>
+    <h1>Hello, Nginx!</h1>
+    <p>We have just configured our Nginx web server on Ubuntu Server!</p>
+</body>
+</html>
+EOF
+
+cd /etc/nginx/sites-enabled
+
+cat << EOF >tutorial
+
+server {
+       listen 81;
+       listen [::]:81;
+
+       server_name example.ubuntu.com;
+
+       root /var/www/tutorial;
+       index index.html;
+
+       location / {
+               try_files $uri $uri/ =404;
+       }
+}
+EOF
+
+sudo service nginx restart
